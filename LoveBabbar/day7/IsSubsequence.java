debc0fe295f62b48e7a88ccb88b0ecbd2a1030f8
sagar <sagar-iitg>
2022-09-24 12:00:03 +0530

    day7

---
+++ LoveBabbar/day7/IsSubsequence.java
@@ -0,0 +1,31 @@
+package day7;
+
+public class IsSubsequence {
+
+
+    public static void main(String[] args) {
+        
+        String str1="abcd";
+        String str2="abd";
+
+       int count= solve(str1,0,str2,0);
+       System.out.println(count);
+    }
+
+    private static int  solve(String s1,int index1,String s2,int index2)
+    {
+
+        //Base case
+        if(s2.length()==index2)
+            return 0;
+
+        if(s1.charAt(index1)==s2.charAt(index2))
+        {
+            return 1+solve(s1, index1+1, s2, index2+1);
+
+        }
+
+        return solve(s1, index1+1, s2, index2);
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Recursion-Backtracking
 String
+
+
+Recursion//DP --Dynamic Programming
+
+1. Subsequence
+2. Longest Increasing Subsequence
+3. Longest Common Subsequences
+4. Longest Arithmetic Subsequence
+5. Dis
+
+In subsequence question always maintain relative ordering
+
+abc
+
+subsequences of "abc"
+
+
+2^3=8 ---> Subsequences
+
+a
+b
+c
+ab
+ac
+bc
+abc
+""
+
+
