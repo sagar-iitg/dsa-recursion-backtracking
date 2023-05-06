036c9eb230925e7dbcb51d498d589380dd2f6d58
sagar <sagar-iitg>
2023-05-06 09:25:02 +0530

    refactoring

--- misc/recursion/SumofNumber.java
+++ misc/recursion/SumofNumber.java
@@ -5,9 +5,11 @@ class SumofNumber{
 
     public static void main(String[] args) {
         
-        int arr[]={-1,2,5,4};
+        int arr[]={1,2,3,4};
         int sum=solve(arr,0,0);
-        System.out.println(sum);
+        int sum1=solve1(arr,0);
+       System.out.println(sum);
+        System.out.println(sum1);
 
     }
     public static int solve(int arr[],int i,int ans)
@@ -19,5 +21,15 @@ class SumofNumber{
         ans=ans+arr[i];
        
         return solve(arr,i+1,ans);
+    }
+    public static int solve1(int arr[],int index)
+    {
+        if(index==arr.length)
+        {
+            return 0;
+        }
+       
+       //sub problems
+        return arr[index]+solve1(arr,index+1);
     }
 }
