406237813659035eede1a36db5ddc54404898714
sagar <sagar-iitg>
2023-05-13 14:40:56 +0530

    print example

--- practice/day1/PrintExample.java
+++ practice/day1/PrintExample.java
@@ -5,11 +5,24 @@ public class PrintExample{
 
     public static void main(String[] args) {
         printIncreasingOrder(7);
-        System.out.println();
+         System.out.println();
         printDecreasingOrder(4);
         System.out.println();
         bothSidePrint(6);
+        System.out.println();
+        bothSidePrintReverse(3);
+        System.out.println();
+        int[] arr={1,2,4,5,0,101};
+        printArray(arr,0);
 
+    }
+    static void printArray(int[] arr,int index)
+    {
+        if(index==arr.length)
+            return;
+
+        System.out.print(arr[index]+" ");
+        printArray(arr, index+1);
     }
     static void bothSidePrint(int n)
     {
@@ -20,6 +33,16 @@ public class PrintExample{
         System.out.print(n+" ");
         bothSidePrint(n-1);
         System.out.print(n+" ");
+ 
+      
+    }
+    static void bothSidePrintReverse(int n)
+    {
+
+        printIncreasingOrder(n);
+       printDecreasingOrder(n);
+     
+ 
       
     }
     static void printIncreasingOrder(int n)
@@ -29,6 +52,7 @@ public class PrintExample{
             return;
         printIncreasingOrder(n-1);
         System.out.print(n+" ");
+       // System.out.println();
     }
     //tail recursion
     static void printDecreasingOrder(int n)
@@ -39,6 +63,7 @@ public class PrintExample{
         System.out.print(n+" ");
         //rest recursion will take care of it
         printDecreasingOrder(n-1);
+        //System.out.println();
     }
     
 }
