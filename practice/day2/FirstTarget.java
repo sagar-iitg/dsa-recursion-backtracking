085b418f72c351d7d4738590f4686dbfd6f5ba53
sagar <sagar-iitg>
2023-05-19 09:44:17 +0530

    array recusrion question

---
+++ practice/day2/FirstTarget.java
@@ -0,0 +1,27 @@
+package day2;
+
+
+
+public class FirstTarget {
+
+
+    public static void main(String[] args) {
+        int arr[]={10,6,3,4,6,6,1,0};
+        int res=solve(arr,0,25);
+        System.out.println(res);
+    }
+
+    static int solve(int arr[],int index,int target)
+    {
+
+
+        if(index==arr.length)
+            return -1;
+        if(arr[index]==target)
+            return index;
+        
+        return solve(arr, index+1, target);
+
+    }
+    
+}
