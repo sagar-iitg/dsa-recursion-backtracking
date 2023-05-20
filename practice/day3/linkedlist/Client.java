da738b832107c90bc10ff370e9eba7c2db143cef
sagar <sagar-iitg>
2023-05-20 21:01:37 +0530

    linked list recursion

---
+++ practice/day3/linkedlist/Client.java
@@ -0,0 +1,24 @@
+package linkedlist;
+
+public class Client {
+    public static void main(String[] args) {
+           
+        LinkedList ll=new LinkedList();
+        
+        ll.addLast(5);
+        ll.addLast(4);
+       // ll.addLast(1);
+        ll.addLast(3);
+        ll.addLast(2);
+        ll.print();
+       // System.out.println( LinkedList.head);
+       
+        ll.reverse();
+        ll.print();
+        //System.out.println( ll.getLength());
+       
+        //int arr[]={2};
+        //ll.printKFromEnd(LinkedList.head,arr);
+    }
+    
+}
---
+++ practice/day3/linkedlist/ClientLinkedList.java
@@ -0,0 +1,63 @@
+package linkedlist;
+
+public class ClientLinkedList {
+
+    public static void main(String[] args) {
+          
+        LinkedList ll=new LinkedList();
+        
+        ll.addLast(5);
+        ll.addLast(4);
+        ll.addLast(1);
+        ll.addLast(3);
+        ll.addLast(2);
+        ll.print();
+
+        System.out.println(LinkedList.head.data);
+        ll.printUsingRecursion(LinkedList.head);
+        System.out.println();
+        ll.printReverseUsingRecursion(LinkedList.head);
+        System.out.println();
+        ll.printUsingRecursion(LinkedList.head);
+
+
+
+     
+
+
+        
+        // ll.head=new Node(1);
+        // ll.head.next=new Node(2);
+       
+      
+        // ll.addLast(1);
+        // ll.addLast(2);
+        // ll.addLast(2);
+        // ll.addLast(1);
+          // ll.addFirst(1);
+          // ll.addLast(2);
+          // ll.addLast(3);
+
+       // ll.print();
+       // System.out.println(ll.recSearch(-1));
+        //System.out.println(ll.checkPalindrome());
+      
+        //ll.print();
+        
+        // ll.reverse();
+        // ll.print();
+      //  ll.deleteNthFromEnd(2);
+        //ll.print();
+
+        //System.out.println(ll.recSearch(4));
+
+        //ll.print();
+        // System.out.println(ll.removeLast());
+        // System.out.println(ll.removeLast());
+        // System.out.println(ll.removeLast());
+        // System.out.println(ll.removeLast());
+        // System.out.println(ll.removeLast());
+
+    }
+    
+}
