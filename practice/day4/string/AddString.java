be907fde770fbfd379a92a0b23dabc825671c259
sagar <sagar-iitg>
2023-05-22 22:06:47 +0530

    string recursion

---
+++ practice/day4/string/AddString.java
@@ -0,0 +1,50 @@
+package string;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class AddString {
+
+    public static void main(String[] args) {
+        
+
+        String a="43";
+        String b="343";
+        //String ans="";
+        ArrayList<String> ans=new ArrayList<>();
+        solve(a, b, 0, ans, a.length()-1, b.length()-1);
+        Collections.reverse(ans);
+        for (String string : ans) {
+            System.out.print(string);
+        }
+        System.out.println();
+        System.out.println(ans);
+
+    }
+    static void solve(String a,String b,int carry,ArrayList<String> ans,int i,int j)
+    {
+
+        if(  i<0 && j<0 && carry==0)
+            return;
+        
+        int first=0,second=0;
+        // if(i>=0)
+        //     first=a.charAt(i)-'0';
+        // if(j>=0)
+        //     second=a.charAt(j)-'0';
+        
+        if(i>=0)
+        first=Character.getNumericValue(a.charAt(i));
+
+    if(j>=0)
+        second=Character.getNumericValue(b.charAt(j));
+
+        int sum=first+second+carry;
+        int lastDigit=sum%10;
+        carry=sum/10;
+        ans.add(lastDigit+"");
+        solve(a, b, carry, ans, i-1, j-1);
+
+    }
+    
+}
---
+++ practice/misc/StringQuestion.java
@@ -0,0 +1,22 @@
+package misc;
+
+public class StringQuestion {
+
+    public static void main(String[] args) {
+        
+
+          // if(i>=0)
+        //     first=a.charAt(i)-'0';
+        // if(j>=0)
+        //     second=a.charAt(j)-'0';
+
+        String str="12";
+        int first=0;
+        first=str.charAt(0)-'0';
+        //System.out.println(str.charAt(0));
+        System.out.println(first);
+
+        System.out.println(Character.getNumericValue(str.charAt(0)));
+    }
+    
+}
