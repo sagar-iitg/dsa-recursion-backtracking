5bb4df8a01afc190ea39e6a556436dadca3ebd3c
sagar <sagar-iitg>
2023-06-25 23:42:43 +0530

    subset

--- backtracking/maze/MazeWithObstacles.java
+++ backtracking/maze/MazeWithObstacles.java
@@ -4,13 +4,13 @@ public class MazeWithObstacles {
 
     public static void main(String[] args) {
         
-            boolean[][] board={{true,true,true},{true,false,true},{true,true,true}};
+            boolean[][] board={{true,true,true},{true,true,true},{true,true,true}};
 
             solve("",board,0,0);
 
     }
 
-    private static void solve(String result, boolean[][] board, int row, int col) {
+    private static void solve(String result,  boolean[][] board, int row, int col) {
 
         if(row==board.length-1 && col==board[0].length-1)
         {
@@ -23,6 +23,9 @@ public class MazeWithObstacles {
             return;
         }
 
+        board[row][col]=false;
+
+
         if(row<board.length-1)
         {
             solve(result+"D", board, row+1, col);
@@ -31,6 +34,17 @@ public class MazeWithObstacles {
         {
             solve(result+"R", board, row, col+1);
         }
+
+        if(row>0)
+        {
+            solve(result+'U', board, row-1, col);
+        }
+        if(col>0)
+        {
+            solve(result+"L", board, row, col-1);
+
+        }
+        board[row][col]=true;
 
 
 
