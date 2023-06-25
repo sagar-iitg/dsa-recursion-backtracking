137406f6e01ab240bfacf46622848288c328b46b
sagar <sagar-iitg>
2023-06-25 20:43:21 +0530

    maze variation2

---
+++ backtracking/maze/MazeVariation2.java
@@ -0,0 +1,32 @@
+package maze;
+
+import java.nio.file.Path;
+
+public class MazeVariation2 {
+
+    public static void main(String[] args) {
+        solve("",3,3);
+        
+    }
+
+    private static void solve(String result, int row, int col) {
+
+        if(row==1 && col==1)
+        {
+            System.out.println(result);
+            return;
+
+        }
+        if(row>1)
+        {
+            solve(result+'D', row-1, col);
+        }
+        if(col>1)
+        {
+            solve(result+'R', row, col-1);
+        }
+
+
+    }
+    
+}
