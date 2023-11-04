17627b20665e3433ce11a62765de50c9c27936a3
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-11-04 13:23:07 +0530

    Created check array is sorted or not

---
+++ src/main/java/com/sagar/arrays/question/CheckArrayIsSortedOrNot1.java
@@ -0,0 +1,19 @@
+package com.sagar.arrays.question;
+
+public class CheckArrayIsSortedOrNot1 {
+    public static void main(String[] args) {
+
+        int[] arr={1,2,3,4,5};
+        boolean ans=sortedArray(arr,0);
+        System.out.println(ans);
+    }
+
+    private static boolean sortedArray(int[] arr, int index) {
+        if (index == arr.length-1) return true;
+
+      return arr[index] <=arr[index + 1] && sortedArray(arr, index + 1);
+
+    }
+
+
+}
