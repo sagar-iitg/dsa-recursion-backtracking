bd0f918dd29ee6961f126106e3d8bbf57a67bc5f
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-11-05 10:49:58 +0530

    Create LinearSearch.java

---
+++ src/main/java/com/sagar/arrays/question/LinearSearch.java
@@ -0,0 +1,20 @@
+package com.sagar.arrays.question;
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+
+        int[] arr={1,2,10,20,7,65};
+        int target=65;
+        boolean ans = solve(arr, target, 0);
+        System.out.println(ans);
+    }
+
+    private static boolean solve(int[] arr, int target,int index) {
+
+        if(index==arr.length) return false;
+
+        if (arr[index]==target) return true;
+        return solve(arr,target,index+1);
+    }
+}
