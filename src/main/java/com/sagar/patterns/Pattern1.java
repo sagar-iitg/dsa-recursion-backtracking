e3a5d974a54b3d47f1232b655bacd28cacbc8f5d
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-11-06 00:41:51 +0530

    Create Pattern1.java

---
+++ src/main/java/com/sagar/patterns/Pattern1.java
@@ -0,0 +1,21 @@
+package com.sagar.patterns;
+
+public class Pattern1 {
+
+    public static void main(String[] args) {
+        solve(4,0);
+    }
+
+    private static void solve(int r, int c) {
+        if(r==0) return;
+        if(c<r) {
+            System.out.print("*");
+            solve(r,c+1);
+        }
+        else{
+            System.out.println();
+            solve(r-1,0);
+
+        }
+    }
+}
