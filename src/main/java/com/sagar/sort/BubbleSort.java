f33e584d2826a4b970841a7c651787fe4bfeba95
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-11-06 01:02:16 +0530

    Create BubbleSort.java

---
+++ src/main/java/com/sagar/sort/BubbleSort.java
@@ -0,0 +1,35 @@
+package com.sagar.sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+
+        int[] arr={4,3,2,1};
+        bubbleSort(arr,3,0);
+        System.out.println(Arrays.toString(arr));
+        //Arrays.stream(arr).forEach(System.out::println);
+    }
+
+    private static void bubbleSort(int[] arr, int r, int c) {
+
+        if(r==0) return;
+        if(c<r)
+        {
+            if(arr[c]>arr[c+1])
+            {
+                //swap
+                int temp=arr[c];
+                arr[c]=arr[c+1];
+                arr[c+1]=temp;
+            }
+            bubbleSort(arr,r,c+1);
+        }
+        else {
+            bubbleSort(arr,r-1,0);
+        }
+
+    }
+}
