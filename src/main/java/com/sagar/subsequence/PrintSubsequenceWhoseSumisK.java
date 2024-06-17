7e1a88ea31ec92f41a9d2e58bb7a1866ffc8a3de
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-06-17 17:46:14 +0530

    Create PrintSubsequenceWhoseSumisK.java

---
+++ src/main/java/com/sagar/subsequence/PrintSubsequenceWhoseSumisK.java
@@ -0,0 +1,33 @@
+package com.sagar.subsequence;
+
+import java.util.ArrayList;
+
+public class PrintSubsequenceWhoseSumisK {
+
+    public static void main(String[] args) {
+        int[] arr={17, 18, 6, 11, 2, 4};
+        int sum=6;
+        solve(arr,0,sum,0,new ArrayList<Integer>());
+    }
+
+    private static void solve(int[] arr, int index, int sum, int s,ArrayList<Integer> temp) {
+        if(index==arr.length)
+        {
+            if(s==sum){
+                System.out.println(temp);
+
+            }
+            return;
+        }
+        //include
+        temp.add(arr[index]);
+        s=s+arr[index];
+        solve(arr,index+1,sum,s,temp);
+        //exclude
+        temp.remove(Integer.valueOf(arr[index]));
+        s=s-arr[index];
+        solve(arr,index+1,sum,s,temp);
+
+
+    }
+}
