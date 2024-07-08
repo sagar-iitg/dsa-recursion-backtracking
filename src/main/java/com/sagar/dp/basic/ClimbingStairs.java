af9543799c3345327333bdb8bc0541f37c4e152a
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-07-08 10:35:40 +0530

    Update ClimbingStairs.java

--- src/main/java/com/sagar/dp/basic/ClimbingStairs.java
+++ src/main/java/com/sagar/dp/basic/ClimbingStairs.java
@@ -4,6 +4,10 @@ public class ClimbingStairs {
     public static void main(String[] args) {
         int ans=solve(44);
         System.out.println(ans);
+        int ans1=solve1(44,0);
+        System.out.println(ans1);
+        int a=3/4;
+        System.out.println(a);
     }
 
     public static int solve(int n) {
@@ -13,5 +17,11 @@ public class ClimbingStairs {
         int right=solve(n-2);
         return left+right;
 
+    }
+    public  static int solve1(int n,int i){
+
+        if(i==n) return 1;
+        if(i>n) return 0;
+        return solve1(n,i+1)+solve1(n,i+2);
     }
 }
